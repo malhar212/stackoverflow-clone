a17c49a1e45047ec76a66f970ae0dd72e458dce9
Sam Zilli <sam@Sams-iMac.local>
2023-12-03 12:38:03 -0500

    created users schema

---
+++ server/models/users.js
@@ -0,0 +1,26 @@
+// User Document Schema
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const User = new Schema({
+    username: { 
+        type: String,
+        required : true,
+        maxlength: 20,
+     },
+    email: {
+        type: String,
+        required : true,
+
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+
+});
+
+ module.exports = mongoose.model('User', User);
