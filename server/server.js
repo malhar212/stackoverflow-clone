6bfc42f0f0f02bfc7f78212afe580ad09f3e9b83
Sam Zilli <sam@Sams-iMac.local>
2023-12-03 15:38:11 -0500

    updated user schema

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const port = 8000;
 var cors = require('cors');
 var bodyParser = require('body-parser');
 
+require("dotenv").config();
+
 // Initialize mongo db connection
 require('./config/database');
 
@@ -27,3 +29,52 @@ app.use('/tags', tagRoutes);
 app.listen(port, ()=> {
   console.log('Server running on port 8000');
 });
+
+
+// mongoose
+//   .connect(MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB is  connected successfully"))
+//   .catch((err) => console.error(err));
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:4000"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://sam:bPHET2NvtdXlhqzq@atlascluster.yqrsvmn.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
