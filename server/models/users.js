28fa8d9bbc35de6ac58af58e8a145aeca0e57e78
Sam Zilli <sam@Sams-iMac.local>
2023-12-03 13:19:01 -0500

    fixed errors

--- server/models/users.js
+++ server/models/users.js
@@ -1,14 +1,11 @@
-// User Document Schema
-// https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
-
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); 
-const SALT_WORK_FACTOR = 10;
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new Schema({
     username: { type: String, required: true, index: { unique: true } },
-    email: { type: String, required: true, index: { unique: true } },
+    email: { type: String, required: true, index: {unique: true }},
     password: { type: String, required: true }
 });
 
@@ -41,3 +38,4 @@ UserSchema.methods.comparePassword = function(candidatePassword, cb) {
 };
 
 module.exports = mongoose.model('User', UserSchema);
+
