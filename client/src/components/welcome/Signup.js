a7f5090c3bb4777b1bdc86f8296ef56a97063613
Sam Zilli <sam@Sams-iMac.local>
2023-12-06 16:30:30 -0500

    added constraints on email while signing up

--- client/src/components/welcome/Signup.js
+++ client/src/components/welcome/Signup.js
@@ -3,9 +3,8 @@ import { ToastContainer, toast } from "react-toastify";
 import { DataDao } from '../../models/ModelDAO';
 import { useLocationContext } from '../locationContext';
 
-function Signup({handleButtonClick}) {
+function Signup({ handleButtonClick }) {
   const { setPageAndParams, setLoggedIn } = useLocationContext();
-  console.log("in signup")
 
   const [inputValue, setInputValue] = useState({
     email: "",
@@ -34,20 +33,32 @@ function Signup({handleButtonClick}) {
       position: "bottom-left",
     });
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-    
-      // Check if any field is blank
-      if (!email || !username || !password || !confirmPassword) {
-        handleError("Please fill in all fields");
-        return;
-      }
-    
-      // Check if passwords match
-      if (password !== confirmPassword) {
-        handleError("Passwords do not match");
-        return;
-      }
+  const isEmailValid = (email) => {
+    // Regular expression for a valid email address
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if any field is blank
+    if (!email || !username || !password || !confirmPassword) {
+      handleError("Please fill in all fields");
+      return;
+    }
+
+    // Check if email is valid
+    if (!isEmailValid(email)) {
+      handleError("Please enter a valid email address");
+      return;
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      handleError("Passwords do not match");
+      return;
+    }
 
     const credentials = {
       email,
@@ -81,50 +92,50 @@ function Signup({handleButtonClick}) {
     <div className="form">
       <h2>Signup for Fake Stack Overflow!</h2>
       <form>
-      <div className="buttonColumn">
-        <div>
-          <label htmlFor="email">Email       </label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder="Enter your email"
-            onChange={handleOnChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="username">Username     </label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Enter your username"
-            onChange={handleOnChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password   </label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={handleOnChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={confirmPassword}
-            placeholder="Confirm your password"
-            onChange={handleOnChange}
-          />
-        </div>
-        <button type="submit" onClick={handleSubmit}>Sign Up!</button>
-        <button id='loginButton' onClick={handleButtonClick}>Already signed up? Login here</button>
-        <button type='submit' id='guestButton' href='' onClick={(e) => { e.preventDefault(); setLoggedIn(false); setPageAndParams('questions') }}>Guest</button>
+        <div className="buttonColumn">
+          <div>
+            <label htmlFor="email">Email       </label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="username">Username     </label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              placeholder="Enter your username"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password   </label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              placeholder="Confirm your password"
+              onChange={handleOnChange}
+            />
+          </div>
+          <button type="submit" onClick={handleSubmit}>Sign Up!</button>
+          <button id='loginButton' onClick={handleButtonClick}>Already signed up? Login here</button>
+          <button type='submit' id='guestButton' href='' onClick={(e) => { e.preventDefault(); setLoggedIn(false); setPageAndParams('questions') }}>Guest</button>
         </div>
       </form>
       <ToastContainer />
